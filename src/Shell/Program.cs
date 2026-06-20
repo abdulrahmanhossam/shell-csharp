4af8004707249f58567c2ce79d170affbf0f7e86
abdulrahmanhossam <abdulrahmn398@outlook.com>
2026-06-20 18:53:33 +0300

    Refactor main loop to include command prompt within the loop

--- src/Shell/Program.cs
+++ src/Shell/Program.cs
@@ -2,9 +2,12 @@
 {
     static void Main()
     {
-        Console.Write("$ ");
+        while (true)
+        {
+            Console.Write("$ ");
 
-        var command = Console.ReadLine();
-        Console.WriteLine($"{command}: command not found");
+            var command = Console.ReadLine();
+            Console.WriteLine($"{command}: command not found");
+        }
     }
 }
