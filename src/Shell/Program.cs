24d0b9c34c724878af34a91ab002c49d79771523
abdulrahmanhossam <abdulrahmn398@outlook.com>
2026-06-21 13:44:21 +0300

    Enhance command handling in shell program to include 'type' command and improve input validation

--- src/Shell/Program.cs
+++ src/Shell/Program.cs
@@ -2,15 +2,30 @@
 {
     static void Main()
     {
+        var validCommand = new[] { "echo", "exit", "type" };
+
         while (true)
         {
             Console.Write("$ ");
 
             var command = Console.ReadLine();
-            if (command == "exit")
+
+            if (string.IsNullOrEmpty(command))
+                continue;
+
+            if (command == "exit" || command.StartsWith("exit "))
                 break;
             else if (command!.StartsWith("echo "))
                 Console.WriteLine(command[5..]);
+            else if (command.StartsWith("type "))
+            {
+                string target = command[5..];
+                if (validCommand.Contains(target))
+                    Console.WriteLine($"{target} is a shell builtin");
+                else
+                    Console.WriteLine($"{target}: not found");
+            }
+
             else
                 Console.WriteLine($"{command}: command not found");
         }
